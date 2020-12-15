84b03f862f022423d64c20927803b8f6fe849d87
mr_ <njgriffiths@gmail.com>
2020-12-14 20:19:18 -0800

    dunno. ch-ch-changes

---
+++ dist/main.e74dffe9.chunk.js
@@ -0,0 +1,74 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[0],[
+/* 0 */,
+/* 1 */,
+/* 2 */,
+/* 3 */,
+/* 4 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(0);
+/* harmony import */ var _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var regenerator_runtime_runtime__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(1);
+/* harmony import */ var regenerator_runtime_runtime__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(regenerator_runtime_runtime__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(2);
+/* harmony import */ var _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var vanilla_lazyload__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(3);
+/* harmony import */ var vanilla_lazyload__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(vanilla_lazyload__WEBPACK_IMPORTED_MODULE_3__);
+/* harmony import */ var _css_normalize_css__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(5);
+/* harmony import */ var _css_normalize_css__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_css_normalize_css__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var _css_main_css__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(6);
+/* harmony import */ var _css_main_css__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_css_main_css__WEBPACK_IMPORTED_MODULE_5__);
+
+
+
+// JS
+ // CSS
+
+
+ // assets
+// import placeholder from './images/placeholder.png';
+
+var init = /*#__PURE__*/function () {
+  var _ref = _babel_runtime_helpers_asyncToGenerator__WEBPACK_IMPORTED_MODULE_2___default()( /*#__PURE__*/_babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.mark(function _callee() {
+    var myLazyLoad;
+    return _babel_runtime_regenerator__WEBPACK_IMPORTED_MODULE_0___default.a.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            console.log('hello, world');
+            myLazyLoad = new vanilla_lazyload__WEBPACK_IMPORTED_MODULE_3___default.a();
+            document.addEventListener('DOMContentLoaded', function (ev) {// lazyload images
+              // myLazyLoad.update();
+            });
+
+          case 3:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee);
+  }));
+
+  return function init() {
+    return _ref.apply(this, arguments);
+  };
+}();
+
+init();
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ }),
+/* 6 */
+/***/ (function(module, exports, __webpack_require__) {
+
+// extracted by mini-css-extract-plugin
+
+/***/ })
+],[[4,1,2]]]);
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import normalize from './css/normalize.css';
 import css from './css/main.css';
 
 // assets
-import placeholder from './images/placeholder.png';
+// import placeholder from './images/placeholder.png';
 
 
 const init = async () => {
@@ -15,22 +15,10 @@ const init = async () => {
 
 	document.addEventListener('DOMContentLoaded', (ev) => {
 		// lazyload images
-		myLazyLoad.update();
-		// lazyLoadImages(window, document);
+		// myLazyLoad.update();
 	});
 };
 
 
-// const lazyLoadImages = (w,d) => {
-// 	// https://www.andreaverlicchi.eu/lazyload/
-// 	const b = d.getElementsByTagName('body')[0];
-// 	const s = d.createElement('script'); 
-// 	const v = !('IntersectionObserver' in w) ? '8.7.0' : '10.5.1';
-
-// 	s.async = true;
-// 	s.src = 'https://cdnjs.cloudflare.com/ajax/libs/vanilla-lazyload/' + v + '/lazyload.min.js';
-// 	w.lazyLoadOptions = {}; // Your options here. See 'recipes' for more information about async.
-// 	b.appendChild(s);
-// }
 
 init();
