d826e0cdc82514436d7fb840dc5268be8d7020e6
mr_ <njgriffiths@gmail.com>
2020-12-15 17:01:59 -0800

    basic lightbox working

--- src/index.js
+++ src/index.js
@@ -1,24 +1,60 @@
 // JS
 import LazyLoad from 'vanilla-lazyload';
+// import { PhotoSwipe, PhotoSwipeUI_Default } from 'photoswipe';
+const PhotoSwipe = require('photoswipe');
+const PhotoSwipeUI_Default = require('./js/photoswipe-ui-default.js');
 
 // CSS
-import normalize from './css/normalize.css';
-import css from './css/main.css';
+import './css/normalize.css';
+import './css/main.css';
+import './css/PhotoSwipe.css';
+import './css/default-skin/default-skin.css';
 
-// assets
-// import placeholder from './images/placeholder.png';
+// VARS
+let pswpElement, container;
 
+// IMAGES
+import placeholder from './images/placeholder.png';
 
-const init = async () => {
-	console.log('hello, world');
-	const myLazyLoad = new LazyLoad();
+const images = [
+	{
+        src: 'https://placekitten.com/600/400',
+        w: 1600,
+        h: 400
+    },
+    {
+        src: placeholder,
+        w: 600,
+        h: 400
+    }
+];
 
-	document.addEventListener('DOMContentLoaded', (ev) => {
-		// lazyload images
-		// myLazyLoad.update();
-	});
+// photoswipe options (if needed)
+var options = {
+	bgOpacity: 0.8,
+	counterEl: false,
+	fullscreenEl: false,
+	history: false,
+    index: 0, // start at first slide
+	shareEl: false
 };
 
+const init = async (images) => {
+	const myLazyLoad = new LazyLoad();
+	
+	// images lightbox
+	pswpElement = document.querySelectorAll('.pswp')[0];
+	container = document.querySelector('#image-container');
+
+	// load the lightbox on click
+	container.addEventListener('click', initLightbox);
+};
+
+function initLightbox() {
+	const gallery = new PhotoSwipe(pswpElement, PhotoSwipeUI_Default, images, options);
+	gallery.init();	
+}
 
 
-init();
+
+init(images);
